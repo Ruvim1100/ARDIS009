4b3409f476d248030ddc7c1df446cbacababb3d5
ruvim <ruvim-bozaji@mail.ru>
2024-02-21 14:12:17 +0200

    finished Navbar

--- src/scenes/navbar/Navigation.tsx
+++ src/scenes/navbar/Navigation.tsx
@@ -9,26 +9,26 @@ type Props = {};
 const Navbar = (props: Props) => {
   return (
     <nav>
-      <div className="navbar navbar1">
-        <div className="navbar navbar2">
-          <div className="navbar navbar3">
-            <p className="Logo_text">009.md</p>
-            <div className="navbar navbar4">
-              <div className="navbar Nav_List">
-                <p>Главная</p>
-                <p>О нас</p>
-                <p>Самоубийство</p>
-                <p>Пострадать</p>
-              </div>
-              <div className="navbar">
-                <p>Войти</p>
-                <button>Присоедениться</button>
-              </div>
+    <div className="navbar-container navbar">
+      <div className="navbar-container navbar-content">
+        <div className="navbar-container navbar-menu">
+          <img className="logo" src={Logo} alt="Logo"/>
+          <div className="navbar-container navbar-buttons">
+            <div className="navbar-container nav-list">
+              <p>Главная</p>
+              <p>О нас</p>
+              <p>Самоубийство</p>
+              <p>Пострадать</p>
+            </div>
+            <div>
+              <p>Войти</p>
+              <button>Присоединиться</button>
             </div>
           </div>
         </div>
       </div>
-    </nav>
+    </div>
+  </nav>
   );
 };
 
